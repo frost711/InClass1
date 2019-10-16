14c1e9ebb15e2e73477452fc73de39006e986b3d
Nik Frost <frost.nik26@gmail.com>
2019-10-16 14:13:12 -0500

    Fix test for compareTo

--- StudentTest.java
+++ StudentTest.java
@@ -31,20 +31,33 @@ public class StudentTest {
 		Student secondStudent = new Student("x", "Y", 3);
 		
 		int actual = secondStudent.compareTo(firstStudent);
-		int expected = 1;
 		
-		assertEquals(expected, actual);
 		
+		boolean result = false;
+		
+		// should be bigger than zero
+		if (actual > 0)
+			result = true;
+		assertTrue(result);
+		
+		
+		result = false;
+		
+		// should be less than 0
 		Student student = new Student("x", "Y", 5);
-		
-		expected = -1;
 		actual = secondStudent.compareTo(student);
+		if (actual < 0)
+			result = true;
+		assertTrue(result);
+		
+
+		result = false;
+
+		// should be the same, therefore should be 0
+		Student studentSame = new Student("x", "y", 5);
+		actual = secondStudent.compareTo(studentSame);
+		int expected = 0;
 		assertEquals(expected, actual);
-
-
-		
-		
-		
 
 
 	}
